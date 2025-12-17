dae84967a3cac7fe27cae74928a2b8cc88715250
bienvanphuc <phucbv@flydinotech.com>
2025-12-17 19:30:12 +0700

    update, soft delete prod

--- app/routers/product_router.py
+++ app/routers/product_router.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, Depends
 from app.db.base import get_db
 from sqlalchemy.orm import Session
 from app.models.product_model import Product
-from app.schemas.product_schema import ProductSchema, CreateProductSchema
+from app.schemas.product_schema import ProductSchema, CreateProductSchema, UpdateProductSchema
 from app.schemas.base_schema import DataResponse
+from datetime import datetime
 
 router = APIRouter()
 
 @router.get("/products", tags=["products"], description="Get all products", response_model=DataResponse[list[ProductSchema]])
 async def get_products(db: Session = Depends(get_db)):
-    products = db.query(Product).all()
+    products = db.query(Product).filter(Product.deleted_at == None).all()
     return DataResponse.custom_response(code="200", message="get list products", data=products)
 
 @router.post("/products", tags=["products"], description="Create a new product", response_model=DataResponse[ProductSchema])
@@ -32,6 +33,20 @@ def delete_product(product_id: int, db: Session = Depends(get_db)):
     product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
         return DataResponse.custom_response(code="404", message="Product not found", data=None)
-    db.delete(product)
+    
+    product.deleted_at = datetime.now()
     db.commit()
     return DataResponse.custom_response(code="200", message="Delete product by id", data=None)
+
+@router.put("/products/{product_id}", tags=["products"], description="Update a product by id", response_model=DataResponse[ProductSchema])
+def update_product(product_id: int, data: UpdateProductSchema, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        return DataResponse.custom_response(code="404", message="Product not found", data=None)
+    
+    update_data = data.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(product, key, value)
+    db.commit()
+    db.refresh(product)
+    return DataResponse.custom_response(code="200", message="Update product by id", data=product)
