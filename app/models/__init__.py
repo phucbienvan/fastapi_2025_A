0f01596bbe94f50fd8f59fc3dc3b33d6cc337eb2
bienvanphuc <phucbv@flydinotech.com>
2025-12-17 20:00:25 +0700

    register

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,3 @@
 from app.models.base_model import Base
 from app.models.product_model import Product
+from app.models.user_model import User
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,19 +1,25 @@
 from fastapi import APIRouter
-
+from app.schemas.user_schemas import RegisterUserSchema, UserSchema
+from app.models.user_model import User
+from app.db.base import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.schemas.base_schema import DataResponse
+from app.core.security import hash_password
 router = APIRouter()
 
-@router.get("/users", tags=["users"], description="Get all users")
-async def get_users():
-    return {"message": "Users page"}
 
-@router.post("/users", tags=["users"], description="Create a new user")
-async def create_user():
-    return {"message": "Create user page"}
+@router.post("/register", tags=["users"], description="Register a new user", response_model=DataResponse[UserSchema])
+async def register_user(data: RegisterUserSchema, db: Session = Depends(get_db)):
+    password = hash_password(data.password)
+    user = User(name=data.name, email=data.email, password=password)
+    
+    try:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return DataResponse.custom_response(code="201", message="Register user success", data=user)
+    except Exception as e:
+        return DataResponse.custom_response(code="500", message="Register user failed", data=None)
 
-@router.put("/users", tags=["users"], description="Update a user")
-async def update_user():
-    return {"message": "Update user page"}
 
-@router.delete("/users", tags=["users"], description="Delete a user")
-async def delete_user():
-    return {"message": "Delete user page"}
