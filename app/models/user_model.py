1a07c0bfbf7306c1032f9ad66e7db5ade22e2b3a
VanDuongNgo <ngovanduong.dev@gmail.com>
2025-12-24 18:11:45 +0700

    feat(ngovanduong): implement login function and middleware

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -5,9 +5,8 @@ from datetime import datetime
 class User(BaseModel):
     __tablename__ = "users"
     id: int = Column(Integer, primary_key=True, index=True)
-    name: str = Column(String, index=True)
+    full_name: str = Column(String, index=True)
     email: str = Column(String, index=True)
     password: str = Column(String, index=True)
-    status: int = Column(Integer, index=True, default=1)
+    is_active: bool = Column(Integer, index=True, default=1)
     created_at: datetime = Column(DateTime, index=True, default=datetime.now)
-    updated_at: datetime = Column(DateTime, index=True, default=datetime.now)
