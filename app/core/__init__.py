5165547defe7c8696d0f30557ec7a484bde8e94b
Nguyễn Ngọc Tú Anh <kizunaninosete@gmail.com>
2025-12-24 17:23:06 +0700

    Implement Login function

---
+++ app/core/__init__.py
@@ -0,0 +1,2 @@
+from app.core.config import settings
+from app.core.security import verify_password, hash_password, create_access_token
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,25 +1,55 @@
 from fastapi import APIRouter
-from app.schemas.user_schemas import RegisterUserSchema, UserSchema
+from app.schemas.user_schemas import LoginRequest, LoginSchema, RegisterUserSchema, UserSchema
 from app.models.user_model import User
 from app.db.base import get_db
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from app.schemas.base_schema import DataResponse
-from app.core.security import hash_password
+from app.core.security import create_access_token, hash_password, verify_password
+
 router = APIRouter()
 
 
-@router.post("/register", tags=["users"], description="Register a new user", response_model=DataResponse[UserSchema])
+@router.post(
+    "/register",
+    tags=["users"],
+    description="Register a new user",
+    response_model=DataResponse[UserSchema],
+)
 async def register_user(data: RegisterUserSchema, db: Session = Depends(get_db)):
     password = hash_password(data.password)
     user = User(name=data.name, email=data.email, password=password)
-    
+
     try:
         db.add(user)
         db.commit()
         db.refresh(user)
-        return DataResponse.custom_response(code="201", message="Register user success", data=user)
+        return DataResponse.custom_response(
+            code="201", message="Register user success", data=user
+        )
     except Exception as e:
-        return DataResponse.custom_response(code="500", message="Register user failed", data=None)
+        return DataResponse.custom_response(
+            code="500", message="Register user failed", data=None
+        )
 
 
+@router.post(
+    "/login",
+    tags=["users"],
+    description="Login user",
+    response_model=DataResponse[LoginSchema],
+)
+async def login_user(data: LoginRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == data.email).first()
+    if not user or not verify_password(data.password, user.password):
+        return DataResponse.custom_response(
+            code="401", message="Invalid email or password", data=None
+        )
+    access_token = create_access_token(data={"sub": user.email})
+    login_data = {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user": user
+    }
+
+    return DataResponse.custom_response(code="200", message="Login successful", data=login_data)
